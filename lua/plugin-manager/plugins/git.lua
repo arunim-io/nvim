58fb7d5cb8670373808cfc6541276310f19ab9f3
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2023-08-01 00:02:21 +0600

    add conventional commit support

--- lua/plugin-manager/plugins/git.lua
+++ lua/plugin-manager/plugins/git.lua
@@ -8,4 +8,11 @@ return {
       { '<leader>gg', '<cmd>Neogit<cr>', desc = 'Open Neogit' },
     },
   },
+  {
+    'olacin/telescope-cc.nvim',
+    init = function() require('telescope').load_extension("conventional_commits") end,
+    keys = {
+      { "cg", "<cmd>Telescope conventional_commits<cr>", desc = 'commit using conventional commits' },
+    },
+  },
 }
