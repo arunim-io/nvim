03d6d1a408dbfb98825f1894ce41b3096a9697bf
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2023-08-29 17:09:06 +0600

    fix ruff formatting
    Signed-off-by: Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>

--- lua/lsp/language-servers.lua
+++ lua/lsp/language-servers.lua
@@ -39,14 +39,37 @@ function M.setup_flutter_tools(lsp)
   }
 end
 
+lspconfig.eslint.setup {
+  on_attach = function(client, bufnr)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      command = "EslintFixAll",
+    })
+  end,
+}
+
+lspconfig.ruff_lsp.setup {
+  on_attach = function(client, bufnr)
+    local function format()
+      vim.lsp.buf.format()
+      vim.lsp.buf.code_action {
+        context = {
+          only = { 'source.fixAll.ruff' }
+        },
+        apply = true,
+      }
+    end
+
+    vim.keymap.set('n', '<leader>f', format, { desc = 'Format current file' })
+  end
+}
+
 if isNixOS then
   lspconfig.cssls.setup {}
   lspconfig.tailwindcss.setup {}
-  lspconfig.eslint.setup {}
   lspconfig.nil_ls.setup {}
   lspconfig.taplo.setup {}
   lspconfig.pyright.setup {}
-  lspconfig.ruff_lsp.setup {}
   lspconfig.dockerls.setup {}
   lspconfig.svelte.setup {}
   lspconfig.bashls.setup {}
