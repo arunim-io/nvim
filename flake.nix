f1e71871f0750fa9123754afb555db0fd7579425
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2023-09-22 13:51:04 +0600

    fixes
    Signed-off-by: Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>

--- flake.nix
+++ flake.nix
@@ -10,33 +10,17 @@
   outputs = inputs@{ flake-parts, neovim, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
     systems = [ "x86_64-linux" ];
     perSystem = { pkgs, config, system, ... }: {
-      packages =
-        let
-          args = {
-            inherit pkgs;
-            inherit (neovim.packages.${system}) neovim;
-          };
-        in
-        {
-          default = import ./nix args;
-          extras = import ./nix/packages.nix args;
+      packages = {
+        default = import ./nix {
+          inherit pkgs;
+          inherit (neovim.packages.${system}) neovim;
         };
+        extras = import ./nix/packages.nix { inherit pkgs; };
+      };
       apps.default = {
         type = "app";
         program = "${config.packages.default}/bin/nvim";
       };
-    };
-
-    flake.hm = {
-      enable = true;
-      package = inputs.self.packages.default;
-      extraPackages = inputs.self.packages.extras;
-      defaultEditor = true;
-      viAlias = true;
-      vimAlias = true;
-      withNodeJs = true;
-      withPython3 = true;
-      withRuby = false;
     };
   };
 }
