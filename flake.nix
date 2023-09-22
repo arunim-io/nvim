1ff5d2d0675a20a9eeee92205f875b9e7fe77c9e
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2023-09-22 14:05:05 +0600

    cleanup nix flake
    Signed-off-by: Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>

--- flake.nix
+++ flake.nix
@@ -9,14 +9,8 @@
 
   outputs = inputs@{ flake-parts, neovim, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
     systems = [ "x86_64-linux" ];
-    perSystem = { pkgs, config, system, ... }: {
-      packages = {
-        default = import ./nix {
-          inherit pkgs;
-          inherit (neovim.packages.${system}) neovim;
-        };
-        extras = import ./nix/packages.nix { inherit pkgs; };
-      };
+    perSystem = { pkgs, system, config, ... }: {
+      packages.default = neovim.packages.${system}.neovim;
       apps.default = {
         type = "app";
         program = "${config.packages.default}/bin/nvim";
--- nix/default.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, neovim }:
-let
-  packages = import ./packages.nix { inherit pkgs; };
-in
-pkgs.symlinkJoin {
-  name = "neovim";
-  paths = [ neovim ] ++ packages.all;
-}
-
