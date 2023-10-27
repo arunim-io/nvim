9395d34e2301125d40580896db08b3bcc8b805f0
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2023-10-28 01:06:37 +0600

    add linter support
    Signed-off-by: Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -51,4 +51,20 @@ return {
       },
     },
   },
+  {
+    'mfussenegger/nvim-lint',
+    event = 'BufReadPre',
+    init = function()
+      vim.api.nvim_create_autocmd({ "InsertLeave", "BufWritePost" }, {
+        callback = function() require("lint").try_lint() end,
+      })
+    end,
+    config = function()
+      require('lint').linters_by_ft = {
+        bash = { 'shellcheck' },
+        djangohtml = { 'djlint' },
+        sh = { 'shellcheck' },
+      }
+    end,
+  },
 }
