a0f1efeb1fbf65ed17f7930dfe4d74d605335d95
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2023-10-28 01:30:43 +0600

    add snippets & status bar
    Signed-off-by: Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>

--- lua/lsp/cmp.lua
+++ lua/lsp/cmp.lua
@@ -1,9 +1,9 @@
 local lsp_zero = require 'lsp-zero'
+local luasnip = require 'luasnip'
 
 lsp_zero.extend_cmp()
 
 local cmp = require 'cmp'
-local cmp_action = lsp_zero.cmp_action()
 
 local function has_words_before()
   unpack = unpack or table.unpack
@@ -14,8 +14,8 @@ end
 local function cmp_tab_completion(fallback)
   if cmp.visible() then
     cmp.select_next_item()
-    -- elseif luasnip.expand_or_jumpable() then
-    --  luasnip.expand_or_jump()
+  elseif luasnip.expand_or_jumpable() then
+    luasnip.expand_or_jump()
   elseif has_words_before() then
     cmp.complete()
   else
@@ -26,18 +26,21 @@ end
 local function cmp_shift_tab_completion(fallback)
   if cmp.visible() then
     cmp.select_prev_item()
-    -- elseif luasnip.jumpable(-1) then
-    --  luasnip.jump(-1)
+  elseif luasnip.jumpable(-1) then
+    luasnip.jump(-1)
   else
     fallback()
   end
 end
 
+require('luasnip.loaders.from_vscode').lazy_load()
+
 cmp.setup {
   formatting = lsp_zero.cmp_format(),
+  snippet = { expand = function(args) luasnip.lsp_expand(args.body) end },
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
-    --{ name = 'luasnip' },
+    { name = 'luasnip' },
   }, { { name = 'buffer' } }),
   mapping = cmp.mapping.preset.insert {
     ["<CR>"] = cmp.mapping.confirm { select = true },
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,12 +1,20 @@
 return {
   {
     "EdenEast/nightfox.nvim",
-    config=true,
+    config = true,
     init = function() vim.cmd "colorscheme carbonfox" end,
     opts = {
       options = {
         dim_inactive = true,
       },
     },
-  }
+  },
+  -- Status bar
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    opts = {
+      globalstatus = true,
+    },
+  },
 }
