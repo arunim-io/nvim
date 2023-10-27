74b96161aaa7dfc923103576cb6b1a4a1304de6e
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2023-10-28 01:09:57 +0600

    customise lsp options
    Signed-off-by: Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>

--- lua/lsp/cmp.lua
+++ lua/lsp/cmp.lua
@@ -1,14 +1,48 @@
--- Here is where you configure the autocompletion settings.
-local lsp_zero = require('lsp-zero')
+local lsp_zero = require 'lsp-zero'
+
 lsp_zero.extend_cmp()
 
--- And you can configure cmp even more, if you want to.
-local cmp = require('cmp')
+local cmp = require 'cmp'
 local cmp_action = lsp_zero.cmp_action()
 
-cmp.setup({
+local function has_words_before()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+end
+
+local function cmp_tab_completion(fallback)
+  if cmp.visible() then
+    cmp.select_next_item()
+    -- elseif luasnip.expand_or_jumpable() then
+    --  luasnip.expand_or_jump()
+  elseif has_words_before() then
+    cmp.complete()
+  else
+    fallback()
+  end
+end
+
+local function cmp_shift_tab_completion(fallback)
+  if cmp.visible() then
+    cmp.select_prev_item()
+    -- elseif luasnip.jumpable(-1) then
+    --  luasnip.jump(-1)
+  else
+    fallback()
+  end
+end
+
+cmp.setup {
   formatting = lsp_zero.cmp_format(),
-  mapping = cmp.mapping.preset.insert({
-    ['<C-Space>'] = cmp.mapping.complete(),
-  })
-})
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    --{ name = 'luasnip' },
+  }, { { name = 'buffer' } }),
+  mapping = cmp.mapping.preset.insert {
+    ["<CR>"] = cmp.mapping.confirm { select = true },
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<Tab>"] = cmp.mapping(cmp_tab_completion, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(cmp_shift_tab_completion, { "i", "s" }),
+  }
+}
---
+++ lua/lsp/keymaps.lua
@@ -0,0 +1,16 @@
+return {
+  setup_keymaps = function(lsp)
+    lsp.on_attach(function(client, bufnr)
+      lsp.default_keymaps { buffer = bufnr, omit = { '<F2>', '<F3>', '<F4>', 'gl' } }
+
+      local maps = vim.lsp.buf
+      local map = vim.keymap.set
+
+      local function opts(desc) return { desc = desc, buffer = client.buf } end
+
+      map("n", "<leader>f", maps.format, opts 'Format current file')
+      map('n', '<leader>ca', maps.code_action, opts 'Open code action menu')
+      map('n', '<leader>rn', maps.rename, opts 'Rename current word')
+    end)
+  end
+}
