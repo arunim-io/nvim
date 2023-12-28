2ac05e90099d7280bf2c17d1811a1a1908faa16e
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2023-12-28 20:35:44 +0600

    formatters

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,16 +1,16 @@
-local nightfox={ 
-	"EdenEast/nightfox.nvim" ,
+local nightfox = {
+	"EdenEast/nightfox.nvim",
 	lazy = false,
-	priority=1000,
-	config = function() 
-		require("nightfox").setup{
-		options={
-			transparent=true,
-			dim_inactive=true,
-		},
+	priority = 1000,
+	config = function()
+		require("nightfox").setup {
+			options = {
+				transparent = true,
+				dim_inactive = true,
+			},
 		}
-		vim.cmd("colorscheme terafox") 
+		vim.cmd("colorscheme terafox")
 	end,
 }
 
-return {nightfox}
+return { nightfox }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,12 @@
-local M = {
+return {
 	"nvim-treesitter/nvim-treesitter",
 	build = function() require("nvim-treesitter.install").update({ with_sync = true })() end,
 	config = function()
 		require("nvim-treesitter.configs").setup {
 			sync_install = true,
 			auto_install = true,
+			snippet_engine = "luasnip",
+
 		}
 	end,
-	opts = {
-		snippet_engine = "luasnip",
-	},
 }
-
-return { M }
