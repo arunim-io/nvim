83829071ebcbc6e6efdda0776fa33ea922df72e9
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2024-01-24 01:00:39 +0600

    update config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -27,6 +27,7 @@ return {
       require("lsp")
     end,
   },
+  { "lvimuser/lsp-inlayhints.nvim", config = true, event = "LspAttach" },
   {
     "folke/trouble.nvim",
     dependencies = "nvim-tree/nvim-web-devicons",
@@ -62,8 +63,11 @@ return {
     ft = "rust",
     config = function()
       vim.g.rustaceanvim = {
+        inlay_hints = { highlight = "NonText" },
+        tools = { hover_actions = { auto_focus = true } },
         server = {
-          on_attach = function(_, bufnr)
+          on_attach = function(client, bufnr)
+            require("lsp-inlayhints").on_attach(client, bufnr)
             vim.keymap.set("n", "<leader>ca", function()
               vim.cmd.RustLsp("codeAction")
             end, { silent = true, buffer = bufnr })
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,16 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = function()
-		require("nvim-treesitter.install").update({ with_sync = true })()
-	end,
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			sync_install = true,
-			auto_install = true,
-			snippet_engine = "luasnip",
-		})
-	end,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = function()
+      require("nvim-treesitter.install").update({ with_sync = true })()
+    end,
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        sync_install = true,
+        auto_install = true,
+        snippet_engine = "luasnip",
+      })
+    end,
+  },
+  { "calops/hmts.nvim", version = "*", ft = "nix", dependencies = "nvim-treesitter/nvim-treesitter" },
 }
