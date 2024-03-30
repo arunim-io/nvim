2aa7bfb1f4457fadbeda77c7dc4fcbf5b7096aa5
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2024-03-30 23:45:18 +0600

    Add keymaps for window management
    Signed-off-by: Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -7,8 +7,6 @@ local keymaps = {
   { "n", "<Tab>", "v><C-\\><C-N>", { desc = "indent by 1" } },
   { "v", "<S-Tab>", "<gv", { desc = "indent by -1" } },
   { "n", "<S-Tab>", "v<<C-\\><C-N>", { desc = "indent by -1" } },
-  { "n", "<C-Bs>", "db", { desc = "Delete previous word", noremap = true, silent = true } },
-  { "n", "<C-Delete>", "dw", { desc = "Delete next word", noremap = true, silent = true } },
   { "n", "<C-A>", "ggVG", { desc = "Select all", noremap = true, silent = true } },
   { "x", "<leader>p", [["_dP]], { desc = "Paste without losing buffer" } },
   { { "n", "v" }, "<leader>y", [["+y]], { desc = "copy to system clipboard" } },
@@ -20,6 +18,12 @@ local keymaps = {
     [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
     { desc = "Replace text under cursor with regex" },
   },
+  { "n", "<C-Left>", "<C-w>h", { desc = "Move to left window", noremap = true, silent = true } },
+  { "n", "<C-Down>", "<C-w>j", { desc = "Move to bottom window", noremap = true, silent = true } },
+  { "n", "<C-Up>", "<C-w>k", { desc = "Move to top window", noremap = true, silent = true } },
+  { "n", "<C-Right>", "<C-w>l", { desc = "Move to right window", noremap = true, silent = true } },
+  { "n", "<leader>v", "<C-w>v", { desc = "Split window vertically", noremap = true, silent = true } },
+  { "n", "<leader>h", "<C-w>s", { desc = "Split window horizontally", noremap = true, silent = true } },
 }
 
 for _, value in ipairs(keymaps) do
