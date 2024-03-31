165b7353224376cbfb7227be50e67c38ae513bc5
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2024-03-31 19:14:51 +0600

    move lazy loader into `init.lua`

--- init.lua
+++ init.lua
@@ -1,4 +1,20 @@
 require("globals")
 require("options")
 require("keymaps")
-require("load_lazy")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
