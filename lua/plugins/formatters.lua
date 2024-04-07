6a888a58db741b4c30856ae87327266334427286
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2024-04-08 01:34:36 +0600

    use biome as main formatter & prettier as fallback

--- lua/plugins/formatters.lua
+++ lua/plugins/formatters.lua
@@ -18,6 +18,7 @@ return {
   opts = {
     format_on_save = { timeout_ms = 500, lsp_fallback = true },
     formatters_by_ft = {
+      ["_"] = { { "biome", "prettier" } },
       go = { "gofumpt", "goimports", "golines" },
       htmldjango = { "djlint" },
       lua = { "stylua" },
