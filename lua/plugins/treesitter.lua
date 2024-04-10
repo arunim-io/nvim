9671344b64022e3618c77522cce5a15fad4931c7
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2024-04-10 20:50:56 +0600

    update treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,31 +5,14 @@ return {
       require("nvim-treesitter.install").update({ with_sync = true })()
     end,
     dependencies = { "windwp/nvim-ts-autotag" },
-    config = function()
-      local lsp = vim.lsp
-      lsp.handlers["textDocument/publishDiagnostics"] = lsp.with(lsp.diagnostic.on_publish_diagnostics, {
-        underline = true,
-        virtual_text = {
-          spacing = 5,
-          severity_limit = "Warning",
-        },
-        update_in_insert = true,
-      })
-
-      require("nvim-treesitter.configs").setup({
-        auto_install = true,
-        snippet_engine = "luasnip",
-        highlight = { enable = true },
-        incremental_selection = { enable = true },
-        indent = { enable = true },
-        autotag = {
-          enable = true,
-          enable_rename = true,
-          enable_close = true,
-          enable_close_on_slash = true,
-        },
-      })
-    end,
+    opts = {
+      auto_install = true,
+      snippet_engine = "luasnip",
+      highlight = { enable = true },
+      incremental_selection = { enable = true },
+      indent = { enable = true },
+      autotag = { enable = true, enable_close_on_slash = true },
+    },
   },
   {
     "calops/hmts.nvim",
