425cda1ac9d6ba3285390e3891948cfb3e02de58
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2024-06-26 20:35:25 +0600

    configure indent & backet highlighting and pairing plugins
    Signed-off-by: Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>

---
+++ lua/configs/editing.lua
@@ -0,0 +1,45 @@
+local highlight = {
+  "RainbowRed",
+  "RainbowYellow",
+  "RainbowBlue",
+  "RainbowOrange",
+  "RainbowGreen",
+  "RainbowViolet",
+  "RainbowCyan",
+}
+
+local hooks = require("ibl.hooks")
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+  vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+  vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+  vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+  vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+  vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+  vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+  vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+end)
+
+vim.g.rainbow_delimiters = {
+  highlight = highlight,
+}
+
+require("ibl").setup({
+  scope = {
+    highlight = highlight,
+  },
+  indent = {
+    char = "▏",
+  },
+})
+
+hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+
+require("nvim-autopairs").setup({
+  check_ts = true,
+  disable_in_visualblock = true,
+  fast_wrap = {
+    map = "<C-e>",
+  },
+})
+
+require("cmp").event:on("conform_done", require("nvim-autopairs.completion.cmp").on_confirm_done())
