5e42c6bc0eab0dde8d96666469085fd7841aada0
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2024-06-29 14:51:49 +0600

    fixes

--- lua/configs/editing.lua
+++ lua/configs/editing.lua
@@ -20,7 +20,9 @@ hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
   vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
 end)
 
-vim.g.rainbow_delimiters.highlight = highlight
+vim.g.rainbow_delimiters = {
+  highlight = highlight,
+}
 
 require("ibl").setup({
   scope = {
