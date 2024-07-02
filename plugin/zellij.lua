2ab03023ac7c9d84433d70e91c0614250bdd730e
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2024-07-02 11:58:22 +0600

    plugin(zellij): make sure that neovim is currently running in zellij
    Signed-off-by: Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>

--- plugin/zellij.lua
+++ plugin/zellij.lua
@@ -13,13 +13,12 @@ local focus_direction_map = {
 --- Move focus to another neovim buffer or zellij pane
 ---@param direction 'left'|'right'|'up'|'down'
 local function move_focus(direction)
-  -- get window ID, try switching windows, and get ID again to see if it worked
   local cur_winnr = vim.fn.winnr()
   vim.api.nvim_command("wincmd " .. focus_direction_map[direction])
+
   local new_winnr = vim.fn.winnr()
 
-  -- if the window ID didn't change, then we didn't switch
-  if cur_winnr == new_winnr then
+  if cur_winnr == new_winnr and vim.env.ZELLIJ ~= nil then
     vim.fn.system("zellij action move-focus " .. direction)
     if vim.v.shell_error ~= 0 then
       vim.notify("zellij.nvim: `zellij` executable not found in PATH", vim.log.levels.ERROR)
