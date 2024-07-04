319ad5aa2d981158c857b376b6c38c57162ddb96
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2024-07-04 16:55:46 +0600

    add nix flake
    Signed-off-by: Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+  };
+
+  outputs =
+    { self, nightly-overlay, ... }:
+    {
+      overlays.default = nightly-overlay.overlays.default;
+
+      homeManagerModule = self.homeManagerModules.default;
+      homeManagerModules.default = import ./module.nix;
+    };
+
+  nixConfig = {
+    extra-substituters = [ "https://nix-community.cachix.org" ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+}
---
+++ module.nix
@@ -0,0 +1,80 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+let
+  namespace = "arunim.recipes.neovim";
+in
+{
+  options.${namespace}.enable = lib.mkEnableOption "Arunim's NeoVim config";
+
+  config = lib.mkIf config.${namespace}.enable {
+    programs.neovim = {
+      enable = true;
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
+      withPython3 = false;
+      withRuby = false;
+      extraPackages =
+        let
+          LSPkgs = with pkgs; [
+            # nix
+            nil
+            nixfmt-rfc-style
+            statix
+            # lua
+            lua-language-server
+            selene
+            stylua
+            # html,css,json
+            vscode-langservers-extracted
+            emmet-language-server
+            # shell
+            nodePackages.bash-language-server
+            shfmt
+            shellcheck
+            # dockerfile
+            dockerfile-language-server-nodejs
+            # python
+            python312
+            pyright
+            ruff
+            ruff-lsp
+            black
+            # javascript
+            nodejs_20
+            corepack_20
+            typescript
+            nodePackages.svelte-language-server
+            nodePackages.typescript-language-server
+            nodePackages."@astrojs/language-server"
+            nodePackages."@tailwindcss/language-server"
+            biome
+            typescript
+            # multi-lang formatter
+            prettierd
+            # toml
+            taplo
+            # yaml
+            yaml-language-server
+          ];
+
+          systemPkgs = with pkgs; [
+            ripgrep
+            fd
+            fzf
+            wl-clipboard
+            gcc
+            luajit
+            gnumake
+            unzip
+            xdg-utils
+          ];
+        in
+        systemPkgs ++ LSPkgs;
+    };
+  };
+}
