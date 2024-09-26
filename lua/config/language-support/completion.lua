26d4ce253ba0228040ec03ba870b0806256aa29e
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2024-09-26 23:16:44 +0600

    load cmp source according to feature

--- lua/config/language-support/completion.lua
+++ lua/config/language-support/completion.lua
@@ -1,52 +1,51 @@
 local cmp = require("cmp")
 
-cmp.setup.filetype("gitcommit", {
-	sources = cmp.config.sources({
-		{ name = "git" },
-	}, {
-		{ name = "buffer" },
-	}),
-})
-require("cmp_git").setup()
+if nixCats("git") then
+  cmp.setup.filetype("gitcommit", {
+    sources = cmp.config.sources({
+      { name = "git" },
+    }, {
+      { name = "buffer" },
+    }),
+  })
+  require("cmp_git").setup()
+end
 
 cmp.setup.cmdline({ "/", "?" }, {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = {
-		{ name = "buffer" },
-	},
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = "buffer" },
+  },
 })
 
 cmp.setup.cmdline(":", {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = cmp.config.sources({
-		{ name = "path" },
-	}, {
-		{ name = "cmdline" },
-	}),
-	---@diagnostic disable-next-line: missing-fields
-	matching = { disallow_symbol_nonprefix_matching = false },
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = "path" },
+  }, {
+    { name = "cmdline" },
+  }),
+  ---@diagnostic disable-next-line: missing-fields
+  matching = { disallow_symbol_nonprefix_matching = false },
 })
 
 local sources = {
-	{ name = "nvim_lsp" },
-	{ name = "luasnip" },
 }
 
-if nixCats("language-support.lsp") and pcall(require, "lazydev") then
-	table.insert(sources, { name = "lazydev", group_index = 0 })
+if nixCats("language-support.lsp") then
+  table.insert(sources, { name = "nvim_lsp" })
+
+  if pcall(require, "lazydev") then
+    table.insert(sources, { name = "lazydev", group_index = 0 })
+  end
 end
 
 cmp.setup({
-	sources = cmp.config.sources(sources, {
-		{ name = "buffer" },
-	}),
-	mapping = cmp.mapping.preset.insert({
-		["<CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
-		["<C-Space>"] = cmp.mapping.complete(),
-	}),
-	snippet = {
-		expand = function(args)
-			require("luasnip").lsp_expand(args.body)
-		end,
-	},
+  sources = cmp.config.sources(sources, {
+    { name = "buffer" },
+  }),
+  mapping = cmp.mapping.preset.insert({
+    ["<CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+    ["<C-Space>"] = cmp.mapping.complete(),
+  }),
 })
