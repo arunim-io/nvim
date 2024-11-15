7ddb1f90cfe6958a5f9f7f368f4a2dcd7fb47287
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2024-11-15 13:51:27 +0600

    add some plugins from LazyNvim's docs

--- lua/anc/plugins/completion.lua
+++ lua/anc/plugins/completion.lua
@@ -2,7 +2,7 @@ return {
   "saghen/blink.cmp",
   name = "blink-cmp",
   lazy = false,
-  dependencies = { "rafamadriz/friendly-snippets", "echasnovski/mini.icons" },
+  dependencies = { "rafamadriz/friendly-snippets" },
   version = "v0.*",
   --- @type blink.cmp.Config
   opts = {
@@ -10,5 +10,20 @@ return {
     keymap = { preset = "enter" },
     accept = { auto_brackets = { enabled = true } },
     trigger = { signature_help = { enabled = true } },
+    sources = {
+      completion = {
+        enabled_providers = { "lsp", "path", "snippets", "buffer", "lazydev" },
+      },
+      providers = {
+        lsp = {
+          -- dont show LuaLS require statements when lazydev has items
+          fallback_for = { "lazydev" },
+        },
+        lazydev = {
+          name = "LazyDev",
+          module = "lazydev.integrations.blink",
+        },
+      },
+    },
   },
 }
