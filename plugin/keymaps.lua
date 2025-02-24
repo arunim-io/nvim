f1f8b55c76c99dd911cf18a44eea5d5f9f0a55a3
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2025-02-24 20:18:48 +0600

    added code from mini-basics

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -5,13 +5,21 @@ local function map(mode, keymap, cmd, desc, opts)
 end
 
 -- [[ Search ]]
+map(
+  "n",
+  "gV",
+  '"`[" . strpart(getregtype(), 0, 1) . "`]"',
+  "Visually select changed text",
+  { expr = true, replace_keycodes = false }
+)
+map("x", "g/", "<esc>/\\%V", "Search inside visual selection", { silent = false })
 map("n", "<Esc>", "<cmd>nohlsearch<cr>", "Clear highlights on search when pressing <Esc>")
 
 -- [[ Moving lines ]]
-map("v", "<M-Up>", ":m '<-2<CR>gv=gv", "Move selected line down")
-map("v", "<M-Down>", ":m '>+1<CR>gv=gv", "Move selected line up")
-map("n", "<M-Up>", ":m .-2<CR>==", "Move current line up")
-map("n", "<M-Down>", ":m .+1<CR>==", "Move current line down")
+map("v", "<M-Up>", "<cmd>m '<-2<CR>gv=gv", "Move selected line down")
+map("v", "<M-Down>", "<cmd>m '>+1<CR>gv=gv", "Move selected line up")
+map("n", "<M-Up>", "<cmd>m .-2<CR>==", "Move current line up")
+map("n", "<M-Down>", "<cmd>m .+1<CR>==", "Move current line down")
 
 -- [[ Indent lines ]]
 map("v", "<Tab>", ">gv", "indent by 1")
@@ -19,7 +27,7 @@ map("n", "<Tab>", "v><C-\\><C-N>", "indent by 1")
 map("v", "<S-Tab>", "<gv", "indent by -1")
 map("n", "<S-Tab>", "v<<C-\\><C-N>", "indent by -1")
 
--- [[ Clipboard ]]
-map("x", "<leader>p", [["_dP]], "Paste without losing buffer")
-map({ "n", "v" }, "<leader>y", [["+y]], "copy to system clipboard")
-map("n", "<leader>Y", [["+Y]], "copy to system clipboard")
+-- [[ Copy/paste with system clipboard ]]
+map({ "n", "v", "x" }, "gy", '"+y', "copy to system clipboard")
+map("n", "gp", '"+p', "Paste from system clipboard")
+map("x", "gp", '"+P', "Paste from system clipboard")
