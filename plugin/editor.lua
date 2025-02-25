6260e6393307a94cd1192ae6bfc29e2e46ec7672
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2025-02-25 16:52:37 +0600

    add `which-key.nvim` & `grug-far.nvim`
    Signed-off-by: Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>

--- plugin/editor.lua
+++ plugin/editor.lua
@@ -5,3 +5,24 @@ require("mini.splitjoin").setup()
 require("mini.surround").setup()
 require("mini.bracketed").setup()
 require("mini.files").setup()
+
+MiniDeps.add("folke/which-key.nvim")
+
+require("which-key").setup({
+  preset = "helix",
+})
+
+MiniDeps.add("MagicDuck/grug-far.nvim")
+
+local grug_far = require("grug-far")
+
+grug_far.setup()
+
+vim.keymap.set({ "n", "v" }, "<leader>rw", function()
+  local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+
+  grug_far.open({
+    transient = true,
+    prefills = { filesFilter = ext and ext ~= "" and "*." .. ext or nil },
+  })
+end, { desc = "Search & replace" })
