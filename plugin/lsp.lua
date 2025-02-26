09a20082abdd179c3daa2e4546b5cdebb21c32d5
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2025-02-26 12:12:00 +0600

    configure more lsps

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -7,16 +7,62 @@ local servers = {}
 servers.lua_ls = {
   settings = {
     Lua = {
-      workspace = { checkThirdParty = false },
-      codeLens = { enable = true },
       completion = { callSnippet = "Replace" },
       doc = { privateName = { "^_" } },
+      format = { enable = false },
+      workspace = { checkThirdParty = false },
     },
   },
 }
 
+servers.basedpyright = {
+  settings = {
+    basedpyright = { disableOrganizeImports = true },
+    python = {
+      analsysis = {
+        ignore = { "*" },
+      },
+    },
+  },
+}
+
+servers.ruff = {
+  on_attach = function(client)
+    client.server_capabilities.hoverProvider = false
+  end,
+  settings = {
+    ruff = {
+      cmd_env = { RUFF_TRACE = "messages" },
+      init_options = {
+        settings = { logLevel = "error" },
+      },
+    },
+  },
+}
+
+servers.taplo = {
+  settings = {
+    formatter = { allowed_blank_lines = 1 },
+  },
+}
+
+servers.html = {
+  init_options = { provideFormatter = false },
+  on_attach = function(client)
+    client.capabilities.textDocument.completion.completionItem.snippetSupport = true
+  end,
+}
+
+servers.emmet_language_server = {}
+
+servers.cssls = {
+  init_options = { provideFormatter = false },
+}
+
+servers.ts_ls = {}
+
 for name, config in pairs(servers) do
-  config.capabilities=require('blink.cmp').get_lsp_capabilities(config.capabilities, true)
+  config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities, true)
 
   require("lspconfig")[name].setup(config)
 end
