130f677f228f0f991b1a02a9561db7a3d6a263ed
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2025-03-30 22:30:55 +0600

    add some toggles for tailwind-tools

--- plugin/tailwind.lua
+++ plugin/tailwind.lua
@@ -11,6 +11,47 @@ MiniDeps.add({
   },
 })
 
+local tailwind_tools_state = require("tailwind-tools.state")
+
+Snacks.toggle
+  .new({
+    name = "Tailwind Tools Colour highlighting",
+    get = function()
+      return tailwind_tools_state.color.enabled
+    end,
+    set = function()
+      vim.cmd("TailwindColorToggle")
+    end,
+  })
+  :map("<leader>ttc")
+
+Snacks.toggle
+  .new({
+    name = "Tailwind Tools Conceal",
+    get = function()
+      return tailwind_tools_state.conceal.enabled
+    end,
+    set = function()
+      vim.cmd("TailwindConcealToggle")
+    end,
+  })
+  :map("<leader>tth")
+
+local sort_classes = true
+
+Snacks.toggle
+  .new({
+    name = "Tailwind Tools class Sorting",
+    get = function()
+      return sort_classes
+    end,
+    set = function(state)
+      sort_classes = state
+    end,
+  })
+  :map("<leader>tts")
+
+---@diagnostic disable-next-line: missing-fields
 require("tailwind-tools").setup({
   server = {
     --- @param client vim.lsp.Client
