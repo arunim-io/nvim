20249d7b90d6263f441f2c939c620ceaa4a82afd
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2025-04-03 11:04:34 +0600

    add provider for wsl clipboard

--- plugin/options.lua
+++ plugin/options.lua
@@ -120,5 +120,20 @@ vim.diagnostic.config({
   virtual_text = {
     severity = { min = "WARN", max = "ERROR" },
   },
-  -- update_in_insert=false,
 })
+
+--[[ Clipboard ]]
+if vim.fn.has("wsl") then
+  vim.g.clipboard = {
+    name = "WslClipboard",
+    copy = {
+      ["+"] = "clip.exe",
+      ["*"] = "clip.exe",
+    },
+    paste = {
+      ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+  }
+end
