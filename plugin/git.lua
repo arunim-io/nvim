3e949c63c48f26d11cde87d830f8ce3bb916e93c
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2025-06-08 13:38:00 +0600

    update git config

--- plugin/git.lua
+++ plugin/git.lua
@@ -1,2 +1,10 @@
-require("mini.diff").setup()
+require("mini.diff").setup({
+  mappings = {
+    apply = "<leader>gs",
+    reset = "<leader>gr",
+  },
+})
 require("mini.git").setup()
+
+vim.keymap.set("n", "<leader>gS", "<cmd>Git add %<cr>", { desc = "Git add current file" })
+vim.keymap.set("n", "<leader>gR", "<cmd>Git add %<cr>", { desc = "Git remove current file" })
