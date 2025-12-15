834c364da01a6d98b01568bd94a6155d079f0f56
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2025-12-16 04:39:03 +0600

    add helpers for our future code

--- init.lua
+++ init.lua
@@ -27,3 +27,22 @@ require("mini.deps").setup({
     package = package_path,
   },
 })
+
+--- A config table containing useful helpers for configuring neovim
+_G.Config = {}
+
+--- A custom augroup for grouping custom autocmds.
+_G.Config.augroup = vim.api.nvim_create_augroup("custom-config", { clear = true })
+
+--- Helper for creating autocmds
+--- @param event string|string[]
+--- @param opts vim.api.keyset.create_autocmd
+function _G.Config.new_autocmd(event, opts)
+  if opts.group == nil then
+    opts.group = _G.Config.augroup
+  end
+  vim.api.nvim_create_autocmd(event, opts)
+end
+
+--- A helper function to load plugins depending on the case if nvim is started with a file path.
+_G.Config.now_if_args = vim.fn.argc(-1) > 0 and MiniDeps.now or MiniDeps.later
