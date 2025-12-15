564ca1cd3fd2ddd98ad76ef5a72787ebc118b842
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2025-12-16 00:57:23 +0600

    add mini.nvim init script

--- init.lua
+++ init.lua
@@ -4,3 +4,20 @@ vim.g.maplocalleader = " "
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
+
+local package_path = vim.fs.joinpath(vim.fn.stdpath("data"), "/site")
+local mini_path = vim.fs.joinpath(package_path, "/pack/deps/start/mini.nvim")
+if not vim.uv.fs_stat(mini_path) then
+  vim.cmd('echo "Installing `mini.nvim`" | redraw')
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    -- Uncomment next line to use 'stable' branch
+    -- '--branch', 'stable',
+    "https://github.com/nvim-mini/mini.nvim",
+    mini_path,
+  })
+  vim.cmd("packadd mini.nvim | helptags ALL")
+  vim.cmd('echo "Installed `mini.nvim`" | redraw')
+end
